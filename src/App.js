8fb671567ee2101818d7ae65bf927c3fe21798ef
James Aules <james_aules@icloud.com>
2023-02-25 12:39:09 -0500

    Navbar & buttom style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
+import  Button  from 'react-bootstrap/Button';
+import  Nav  from 'react-bootstrap/Nav';
 
 
 
 function App() {
   return (
     <div className="App">
+
+      <Nav activeKey= '/home'>
+        <Nav.Item>
+          <Nav.Link href='/home'>Home </Nav.Link >
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey='/about' >About Us</Nav.Link>
+        </Nav.Item>
+      </Nav>
       <h1>Bootstrap Website</h1>
-     
+      <h2>Hello World!</h2>
+      <Button variant="danger">Danger</Button>{' '}
     </div>
   );
 }
